e5c13f85fc2e2d119b1e1db6201b8d58365d50fb
samtw <sam@infocloud.com.tw>
2017-10-22 20:31:10 +0800

    init record

---
+++ ErrorLogProject/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErrorLogProject.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index(string error)
+        {
+            return View();
+        }
+    }
+}
---
+++ ErrorLogProject/Global.asax.cs
@@ -0,0 +1,66 @@
+﻿using ErrorLogProject.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ErrorLogProject
+{
+    // 注意: 如需啟用 IIS6 或 IIS7 傳統模式的說明，
+    // 請造訪 http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+        }
+
+        protected void Application_Error()
+        {
+            // 當前後台錯誤畫面有芬的時候能從這邊著手
+            //HttpContext con = HttpContext.Current;
+            //con.Request.Url.ToString().ToLower().Contains("backend")
+
+            //取當前錯誤
+            Exception exception = Server.GetLastError();
+            HttpException httpException = exception as HttpException;
+
+            var newRouteData = new RouteData();
+            newRouteData.Values["controller"] = "Error";
+            //等同 --> newRouteData.Values.Add("controller", "Error");  
+            newRouteData.Values["action"] = "Index";
+            newRouteData.Values["ErrorDetail"] = exception.Message;
+      
+            if (httpException != null)
+            {
+                var statusCode = httpException.GetHttpCode();
+                if (statusCode == 404)
+                {
+                    newRouteData.Values["action"] = "PageNotFound";
+                }
+                if (statusCode == 500)
+                {
+                    newRouteData.Values["action"] = "ServerError";
+                }
+                
+            }
+         
+            Response.Clear();
+            //f取消用內建的錯誤
+            Response.TrySkipIisCustomErrors = true;
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), newRouteData));
+        }
+    }
+}
